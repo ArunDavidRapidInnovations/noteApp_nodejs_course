45ca6317038b3cfc397ff9acd25668adb594bc1a
Arun David <davidreddy293@gmail.com>
2021-08-03 10:31:05 +0530

    Added chalk and colored console.log

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const readline = require('readline');
+const chalk = require('chalk');
 
 let filePath = path.join(__dirname, 'fin.csv');
 
@@ -19,13 +20,17 @@ file.on('line', (line) => {
   let data = line.split(',');
   if (data[0] == 'Ex') {
     total_expenses = total_expenses + parseInt(data[1], 10);
+    console.log('Expenses: ' + chalk.red(data[1]));
   } else {
     total_income = total_income + parseInt(data[1], 10);
+    console.log('Income: ' + chalk.green(data[1]));
   }
-  // console.log(parseInt(data[1]));
 });
 
 file.on('close', function () {
-  console.log(total_expenses);
-  console.log(total_income);
+  console.log(chalk.black.bgRed('Total Expenses: ' + total_expenses + 'Rs '));
+  console.log(chalk.black.bgGreen('Total Expenses: ' + total_income + 'Rs '));
+  console.log(
+    chalk.black.bgBlue('Net: ' + (total_income - total_expenses) + 'Rs '),
+  );
 });
